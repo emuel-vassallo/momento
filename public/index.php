4ebc2443f16cec0b6064d296bb2652d8a0071439
emuel-vassallo <emuelvassallo15@gmail.com>
2023-05-24 01:42:11 +0200

    add minimal post caption and image styling

--- public/index.php
+++ public/index.php
@@ -31,19 +31,25 @@ function display_posts($posts)
         $imageDir = $post['image_dir'];
         $caption = $post['caption'];
 
-        echo "<div class='post d-flex flex-column w-100 mb-5 pb-2'>
-                <div class='post-top d-flex align-items-center mb-3'>
-                  <div class='post-user-info d-flex align-items-center justify-content-center'>
-                    <img class='feed-card-profile-picture me-2' src='$poster_profile_picture' alt='$poster_display_name's profile picture'>
-                    <div class='ps-1 d-flex flex-column'>
-                        <p class='m-0 fw-semibold'>$poster_display_name</p>
-                        <p class='m-0 text-dark-emphasis'><small>@$poster_username</small></p>
+        echo "<div class='post d-flex w-100 mb-5 pb-4'>
+                <div class='d-flex flex-column w-100 align-items-start'>
+                    <div class='post-top d-flex align-items-center mb-2 pb-2'>
+                        <div class='post-user-info d-flex align-items-center justify-content-center'>
+                            <img class='feed-card-profile-picture me-2' src='$poster_profile_picture' alt='$poster_display_name's profile picture'>
+                            <div class='ps-1 d-flex flex-column'>
+                                <p class='m-0 fw-semibold'>$poster_display_name</p>
+                                <p class='m-0 text-dark-emphasis'><small>@$poster_username</small></p>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </div>
-                <img class='feed-post-image' src='$imageDir' alt='Post Image'>
-                <div>
-                  <p>$caption</p>
+                    <img class='feed-post-image mb-3 pb-1' src='$imageDir' alt='Post Image'>
+                    <div class='d-flex post-caption align-items-start justify-content-start w-100'>
+                        <p class='m-0 fw-medium'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-quote' viewBox='0 0 16 16'>
+                            <path d='M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z'/>
+                        </svg>
+                        $caption</p>
+                    </div>
                 </div>
               </div>";
     }
@@ -74,8 +80,10 @@ function display_posts($posts)
     <?php include('navbar.php'); ?>
     <main class="d-flex flex-column w-100 h-100">
         <?php include('header.php'); ?>
-        <div class="d-flex feed-container flex-column p-5 align-items-center justify-content-center">
-            <p class="h3">Feed</p>
+        <div class="d-flex feed-container flex-column p-5 align-items-start align-items-center justify-content-center">
+            <div class="feed-top w-100 mb-4">
+                <p class="h3 fw-semibold">Feed</p>
+            </div>
             <div class="feed-posts-container d-flex flex-column align-items-center justify-content-center">
                 <?php display_posts($posts) ?>
             </div>
