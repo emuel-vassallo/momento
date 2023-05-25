59cd55eb7fd7e9d1f3db8c206578497810238145
emuel-vassallo <emuelvassallo15@gmail.com>
2023-05-25 15:09:53 +0200

    make search result options a bootstrap dropdown

--- public/scripts/show-search-suggestions.js
+++ public/scripts/show-search-suggestions.js
@@ -26,9 +26,9 @@ fetch("../core/get_users_list.php")
       let html = "";
       results.forEach((result) => {
         html += `
-          <li class="search-result-item w-100">
+          <li class="search-result-item w-100 rounded">
                 <a href="http://localhost/Emuel_Vassallo_4.2D/instagram-clone/public/user_profile.php?user_id=${result.id}"
-                    class="text-decoration-none w-100 d-flex gap-2 p-2 align-items-center justify-content-start">
+                    class="dropdown-item text-decoration-none w-100 d-flex gap-2 p-2 align-items-center justify-content-start rounded">
               <img class='search-result-profile-picture flex-shrink-0' src='${result.profile_picture_path}' alt='${result.display_name}'s profile picture'>
               <p class="search-result-text text-nowrap m-0 fw-semibold fs-6 overflow-hidden flex-shrink-0 fs-6 text-body">${result.display_name}</p>
               <p class="search-result-text text-nowrap m-0 text-secondary fs-6 overflow-hidden ellipsis fs-6">@${result.username}</p>
