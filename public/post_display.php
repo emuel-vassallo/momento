73b5d8714b6f0ef8bbd936a2038dc626781c6938
emuel-vassallo <emuelvassallo15@gmail.com>
2023-05-25 03:02:29 +0200

    don't show quote icon if it's caption is empty

--- public/post_display.php
+++ public/post_display.php
@@ -33,9 +33,16 @@ function display_posts($posts)
 
         $user_profile_link = "http://localhost/Emuel_Vassallo_4.2D/instagram-clone/public/user_profile.php?user_id=" . $poster_id;
 
+        $caption_html = $caption === '' ? '' : "<p class='post-caption mb-1 fw-medium d-flex align-items-start justify-content-center'>
+                                                    <svg class='bi bi-quote flex-shrink-0 me-1' xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' viewBox='0 0 16 16'>
+                                                        <path d='M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z'/>
+                                                    </svg>
+                                                    $caption
+                                                </p>";
+
         echo "<div class='post d-flex w-100 mb-5 pb-2 bg-white py-4 px-4 border'>
-                <div class='w-100 d-flex flex-column align-items-start'>
-                    <div class='post-top d-flex align-items-center mb-2 pb-2'>
+                <div class='w-100 d-flex flex-column align-items-start gap-3'>
+                    <div class='post-top d-flex align-items-center'>
                         <a href='$user_profile_link' class='text-decoration-none'>
                             <div class='post-user-info d-flex align-items-center justify-content-center'>
                                 <img class='feed-card-profile-picture me-2 flex-shrink-0' src='$poster_profile_picture' alt='$poster_display_name's profile picture'>
@@ -47,14 +54,10 @@ function display_posts($posts)
                         </a>
                     </div>
 
-                    <img class='feed-post-image mb-3 pb-1' src='$post_image_path' alt='Post Image'>
+                    <img class='feed-post-image' src='$post_image_path' alt='Post Image'>
 
                     <div class='d-flex flex-column post-bottom align-items-start justify-content-start w-100'>
-                        <p class='post-caption mb-1 fw-medium d-flex align-items-start justify-content-center'>
-                            <svg class='bi bi-quote flex-shrink-0 me-1' xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' viewBox='0 0 16 16'>
-                            <path d='M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z'/>
-                        </svg>
-                        $caption</p>
+                        $caption_html
                         <p class='post-creation-date text-secondary flex-shrink-0'><small>$time_ago</small></p>
                     </div>
                 </div>
