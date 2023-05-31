9d62f490ab956e75a309964f5f5ebafae1a8fd51
emuel-vassallo <emuelvassallo15@gmail.com>
2023-05-31 05:23:49 +0200

    refactor lazy-load.js: improve readability & organisation

--- public/scripts/lazy-load.js
+++ public/scripts/lazy-load.js
@@ -1,47 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+document.addEventListener("DOMContentLoaded", () => {
+  const lazyImages = [...document.querySelectorAll("img.lazy")];
+  const lazyProfilePictures = [
+    ...document.querySelectorAll(".feed-card-profile-picture.lazy"),
+  ];
 
   if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function (
-      entries,
-      observer
-    ) {
-      entries.forEach(function (entry) {
+    const lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
         if (entry.isIntersecting) {
-          let lazyImage = entry.target;
+          const lazyImage = entry.target;
           lazyImage.src = lazyImage.dataset.src;
           lazyImage.classList.remove("lazy");
           lazyImageObserver.unobserve(lazyImage);
         }
-      });
+      }
     });
 
-    lazyImages.forEach(function (lazyImage) {
-      lazyImageObserver.observe(lazyImage);
-    });
-  }
-
-  let lazyProfilePictures = [].slice.call(
-    document.querySelectorAll(".feed-card-profile-picture.lazy")
-  );
-
-  if ("IntersectionObserver" in window) {
-    let lazyProfilePictureObserver = new IntersectionObserver(function (
-      entries,
-      observer
-    ) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          let lazyProfilePicture = entry.target;
-          lazyProfilePicture.src = lazyProfilePicture.dataset.src;
-          lazyProfilePicture.classList.remove("lazy");
-          lazyProfilePictureObserver.unobserve(lazyProfilePicture);
+    const lazyProfilePictureObserver = new IntersectionObserver(
+      (entries, observer) => {
+        for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+          if (entry.isIntersecting) {
+            const lazyProfilePicture = entry.target;
+            lazyProfilePicture.src = lazyProfilePicture.dataset.src;
+            lazyProfilePicture.classList.remove("lazy");
+            lazyProfilePictureObserver.unobserve(lazyProfilePicture);
+          }
         }
-      });
-    });
+      }
+    );
 
-    lazyProfilePictures.forEach(function (lazyProfilePicture) {
-      lazyProfilePictureObserver.observe(lazyProfilePicture);
-    });
+    for (let i = 0; i < lazyImages.length; i++) {
+      lazyImageObserver.observe(lazyImages[i]);
+    }
+    
+    for (let i = 0; i < lazyProfilePictures.length; i++) {
+      lazyImageObserver.observe(lazyProfilePictures[i]);
+    }
+
+    return;
   }
+
+  console.error("IntersectionObserver is not supported by this browser.");
 });
