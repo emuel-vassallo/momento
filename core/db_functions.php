d57194957deadcb458682b834069758f8541149e
emuel-vassallo <emuelvassallo15@gmail.com>
2023-06-29 15:24:54 +0200

    fix users allowed to log in with hashed password

--- core/db_functions.php
+++ core/db_functions.php
@@ -117,7 +117,7 @@ function get_user_by_credentials($pdo, $username, $password)
 
     $hashed_password = $row['password'];
 
-    if (password_verify($password, $hashed_password) || $password === $hashed_password) {
+    if (password_verify($password, $hashed_password)) {
         return $row;
     }
 
